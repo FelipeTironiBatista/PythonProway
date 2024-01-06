60f95713f6e7327bea5b152ebafe3daa583c1f3b
Felipe Tironi Batista <felipetironibatista@gmail.com>
2024-01-06 18:09:43 -0300

    Subindo aulas adicionais

---
+++ Modulo01/Aula08.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite o primeiro número >> "))
+n2 = int(input("Digite o segundo número >> "))
+
+soma = n1 + n2
+print(f"A primeira variavel é do tipo {type(n1)} e o valor da primeira variavel é {n1}")
+print(f"A primeira variavel é do tipo {type(n2)} e o valor da primeira variavel é {n2}")
+print(f"O tipo primitdo do valor é {type(soma)}")
---
+++ Modulo01/Aula09.py
@@ -0,0 +1,6 @@
+pera = input("Digite qualquer coisa >> ")
+
+print("tem espaco", pera.isspace())
+print("tem caracter numerico", pera.isnumeric())
+print("contem caracter alfabetico", pera.isalpha())
+print("contem caracter alfanumerico", pera.isalnum())
